cba9ab4012813ff3a5c8d42d912d8c5533fa6e0f
CALEB_LATITUDE\jpark <jpark1992@gmail.com>
2024-09-05 11:06:46 +0900

    Numpy unfuc Added.

--- hello.py
+++ hello.py
@@ -22,7 +22,7 @@ print('arr1 + arr2 : {arr3}')
 
 
 print(f'-------------------------------')
-print(f'Numpy Operation Example')
+print(f'Numpy Slicing Example')
 
 # Slicing Numpy Arrays
 np1 = np.array([1,2,3,4,5,6,7,8,9])
@@ -57,4 +57,29 @@ print(np2[1,2])
 print(np2[0:1, 1:3])
 
 
+
+print(f'-------------------------------')
+print(f'Numpy Slicing Example')
+# Numpy Universal Function
+np1 = np.array([-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+# Square root of each element
+#print(np.sqrt(np1))
+
+# Absolute Value
+#print(np.absolute(np1))
+
+# Exponents
+#print(np.exp(np1))
+
+# Min/Max
+#print(np.max(np1))
+#print(np.min(np1))
+
+# Sign positive or negative
+#print(np.sign(np1))
+
+# Trig sin cos log
+print(np.log(np1))
 
