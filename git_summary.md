4e65de12485df45c9ff0f0040f4a06b14b8af203
CALEB_LATITUDE\jpark <jpark1992@gmail.com>
2024-09-05 10:31:43 +0900

    hello.py imported Pandas.

--- git_summary.md
+++ git_summary.md
@@ -5,5 +5,3 @@
 * add : commit 을 하기 위해 stage 에 올린다. 
 * commit : 로컬컴퓨터에 저장한다. 
 * push : 원격저장소에 업로드 한다. 
-
-
--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 import numpy as np 
-
+import pandas as pd 
 
 print(np.__version__) 
+print(pd.__version__)
+
+print('hello world!!')
