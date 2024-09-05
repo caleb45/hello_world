c825a67d79507ea96c6127be129768f931e46a23
CALEB_LATITUDE\jpark <jpark1992@gmail.com>
2024-09-05 10:58:04 +0900

    Pandas Dataframe Generated.

--- hello.py
+++ hello.py
@@ -13,4 +13,10 @@ arr2 = np.array(range(1, 11, 2))
 print(f'arr1 : {arr1}')
 print(f'arr2 : {arr2}')
 
+print(f'--------------------')
+print(f'Pandas Code Added')
+
+df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6]]).T, columns=['A', 'B', 'C'])
+print('dataframe : ')
+print(df)
 
