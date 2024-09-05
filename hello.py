f4e29638c1e2b11949a1e1aaffbf88852c1171c5
CALEB_LATITUDE\jpark <jpark1992@gmail.com>
2024-09-05 10:34:00 +0900

    Array Operation Added.

--- hello.py
+++ hello.py
@@ -5,3 +5,12 @@ print(np.__version__)
 print(pd.__version__)
 
 print('hello world!!')
+
+
+arr1 = np.array(range(1, 11))
+arr2 = np.array(range(1, 11, 2))
+
+print(f'arr1 : {arr1}')
+print(f'arr2 : {arr2}')
+
+
